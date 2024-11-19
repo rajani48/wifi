8e46526fbbbf56566848b26341760bd509a807f8
Rajani Nagaraju <rajaninagaraju48@gmail.com>
2024-11-19 21:40:26 +0000

    user space wifi_debug file
    Signed-off-by: Rajani Nagaraju <rajaninagaraju48@gmail.com>

---
+++ wifi_dbg.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINES 100000
+#define LINE_LENGTH 128
+
+int main()
+{
+    FILE *fd = NULL;
+    char line[LINE_LENGTH + 1];
+    int lc = 0;
+
+    fd = fopen("/dev/chdbg", "r");
+    if (fd == NULL) {
+        printf("File open error\n");
+	return 1;
+    }
+
+    while (lc < MAX_LINES) {
+        if (fgets(line, sizeof(line), fd) != NULL) {
+            line[LINE_LENGTH] = '\0';
+            printf("%s", line);
+	    lc ++;
+        } else {
+            printf("End of file\n");
+	    return 0;
+        }
+    }
+
+    fclose(fd);
+    return 0;
+}
