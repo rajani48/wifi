5397d831585fe07ba5e67b55d0c9db050989906f
rajani48 <rajaninagaraju48@gmail.com>
2024-11-26 11:17:32 +0530

    Update sample_dep.h

--- sample/sample_dep.h
+++ sample/sample_dep.h
@@ -12,24 +12,11 @@
 
 #include "chdev_drv.h"
 
-typedef void (*callout_cb)(void *);
-
-static inline void
-callout_reset_dbg(struct callout *my_callout, int hz, callout_cb cb, void *arg)
-{
-    char buf[128];
-    snprintf(buf, 128, "%s is activating timer callout=%p, ticks=%d cb=%p arg=%p"
-                    , __func__, (void*)my_callout, hz, (void*)cb, (void*) arg);
-    wifi_dbg(buf, __func__);
-    callout_reset(my_callout, hz, cb, arg);
+#define callout_reset_dbg(my_callout, hz, cb, arg) {	\
+	char buf[128];					\
+	snprintf(buf, 128, "%s is activating timer", __func__);	\
+	wifi_dbg(buf, "callout=" #my_callout ", ticks=" #hz ", cb=" #cb ", arg=" #arg); \
+	callout_reset(my_callout, hz, cb, arg);		\
 }
-
-#ifdef callout_reset
-#undef callout_reset
-#endif
-
-#define callout_reset(my_callout, hz, cb, arg) \
-        callout_reset_dbg(my_callout, hz, cb, arg)
-
 
 #endif /* _SAMPLE_DEP_H_ */
