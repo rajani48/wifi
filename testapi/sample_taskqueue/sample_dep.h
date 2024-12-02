7e2e15d1a70065166e61ead63d4ce62ec4fafa14
rajani48 <rajani48@gmail.com>
2024-12-02 18:53:49 +0530

    macro definition chnaged

--- testapi/sample_taskqueue/sample_dep.h
+++ testapi/sample_taskqueue/sample_dep.h
@@ -10,15 +10,6 @@
 #include <sys/malloc.h>
 #include <sys/taskqueue.h>
 #include "chdev_drv.h"
-/*
-#define taskqueue_enqueue_dbg(my_tq, my_task) { \
-        char buf[128]; \
-        snprintf(buf, 128, "%s is activating timer", __func__); \
-        wifi_dbg(buf, "taskqueue=" #my_tq ", my_task=" #my_task); \
-	taskqueue_enqueue((my_tq), (my_task)); \
-}
-
-*/
 #define taskqueue_enqueue_dbg(tq, task) {  \
         char buf[128]; \
         snprintf(buf, 128, "%s is activating timer", __func__); \
