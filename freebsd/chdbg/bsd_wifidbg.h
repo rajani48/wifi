6490b4d2e95e59fbc770b7a1fc5e6ddc54ff0566
rajani48 <rajani48@gmail.com>
2024-12-03 12:55:01 +0530

    Submit char device driver for freebsd to debug wifi

---
+++ freebsd/chdbg/bsd_wifidbg.h
@@ -0,0 +1,58 @@
+#ifndef _SAMPLE_DEP_H_
+#define _SAMPLE_DEP_H_
+
+#include <sys/types.h>
+#include <sys/param.h> 
+#include <sys/kernel.h>
+#include <sys/module.h>
+#include <sys/sysctl.h>
+#include <sys/systm.h>
+#include <sys/malloc.h>
+#include <sys/malloc.h>
+#include <sys/mutex.h>
+#include <sys/taskqueue.h>
+
+#include "chdev_drv.h"
+
+//linux header
+#include <linux/init.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
+#include <linux/delay.h>
+#include <linux/workqueue.h>
+
+#define schedule_delayed_work_dbg(my_delaywork, expire) {	\
+	char buf[128];					\
+	snprintf(buf, 128, "%s is activating timer", __func__);	\
+	wifi_dbg(buf, "schedulw_delayed_work=" #my_delaywork ", expire=" #expire); \
+	schedule_delayed_work(my_delaywork, expire);		\
+}
+
+#define mod_timer_dbg(my_timer, expire) {	\
+	char buf[128];					\
+	snprintf(buf, 128, "%s is activating timer", __func__);	\
+	wifi_dbg(buf, "mod_timer=" #my_timer ", expire=" #expire); \
+	mod_timer(my_timer, expire);		\
+}
+
+#define mtx_lock_dbg(my_mutex) {	\
+	char buf[128];					\
+	snprintf(buf, 128, "%s is activating timer", __func__);	\
+	wifi_dbg(buf, "mtx_lock=" #my_mutex); \
+	mtx_lock(my_mutex);		\
+}
+
+#define mtx_unlock_dbg(my_mutex) {	\
+	char buf[128];					\
+	snprintf(buf, 128, "%s is activating timer", __func__);	\
+	wifi_dbg(buf, "mtx_unlock=" #my_mutex); \
+	mtx_unlock(my_mutex);		\
+}
+
+#define taskqueue_enqueue_dbg(tq, task) {  \
+        char buf[128]; \
+        snprintf(buf, 128, "%s is activating timer", __func__); \
+        wifi_dbg(buf, "taskqueue=" #tq ", my_task=" #task); \
+        return taskqueue_enqueue((tq), (task));  \
+}
+#endif /* _SAMPLE_DEP_H_ */
